868f866ec7f676d74c5c9a31ddc06ec8ae65bda7
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-03 00:03:21 +0900

    getPostData

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,9 @@
 import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+}
 
 export default function Post() {
   return <Layout>...</Layout>;
