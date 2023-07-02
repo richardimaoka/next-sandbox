21f725d5830aa217b28162f1609bf3b9870da79e
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-03 03:52:10 +0900

    emotion settings in Next.js

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,9 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { css } from "@emotion/react";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
+const Child = () => {
+  return (
+    <p
+      css={css`
+        background-color: blue;
+      `}
+    >
+      child
+    </p>
+  );
+};
 
 export default function Home() {
   return (
@@ -15,18 +27,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <Child />
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
+
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -110,5 +124,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
