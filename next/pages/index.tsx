a4a6867fc0f2afcabfff1cba15b67b424cc2c121
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-03 04:56:13 +0900

    modal

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,6 +1,10 @@
 import { css } from "@emotion/react";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import { Noto_Sans_JP } from "next/font/google";
+const notoSansJP = Noto_Sans_JP({
+  // Japanese font needs this settings, as index.d.ts doesn't allow subsets = japanese, which is probably due to the large size of japanese font
+  preload: false, // removing this will cause error for missing subsets
+  display: "swap", // removing this will unapplied japanese font, BUT THIS CAUSES LAYOUT SHIFT...!!!
+});
 
 const Child = () => {
   return (
@@ -16,8 +20,30 @@ const Child = () => {
 
 export default function Home() {
   return (
-    <>
-      <Child />
-    </>
+    <div className={notoSansJP.className}>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <div
+          css={css`
+            max-width: 600px;
+          `}
+        >
+          <div
+            css={css`
+              padding: 8px;
+              background: rgba(255, 255, 255, 0.8);
+              box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+            `}
+          >
+            よって、ここに、国連総会は すべての人間は
+            加盟国は、国際連合と協力して
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
