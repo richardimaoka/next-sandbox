8a6f05205c0ee08497e0f74574c38d1f0570f99a
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-03 07:24:28 +0900

    main container

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,49 +1,52 @@
 import { css } from "@emotion/react";
 import { Noto_Sans_JP } from "next/font/google";
+import { ReactNode } from "react";
 const notoSansJP = Noto_Sans_JP({
   // Japanese font needs this settings, as index.d.ts doesn't allow subsets = japanese, which is probably due to the large size of japanese font
   preload: false, // removing this will cause error for missing subsets
   display: "swap", // removing this will unapplied japanese font, BUT THIS CAUSES LAYOUT SHIFT...!!!
 });
 
-const Child = () => {
-  return (
-    <p
+const MobileScreenshot = () => <div>mobile screenshot</div>;
+
+interface MainContainerProps {
+  children: ReactNode;
+}
+
+const MainContainer = ({ children }: MainContainerProps) => (
+  // four main purposes of the container:
+  // 1. set the width
+  // 2. center the container
+  // 3. top and bottom spacing
+  // 4. background color or image <- this is the only thing to set in the outer container element
+  <main>
+    <div
       css={css`
-        background-color: blue;
+        margin: 0 auto;
+        max-width: 968px;
       `}
     >
-      child
-    </p>
-  );
-};
+      {children}
+    </div>
+  </main>
+);
 
 export default function Home() {
   return (
     <div className={notoSansJP.className}>
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-        `}
-      >
+      <MainContainer>
         <div
           css={css`
-            max-width: 600px;
+            padding: 8px;
+            background: rgba(255, 255, 255, 0.8);
+            box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+            width: 100%;
           `}
         >
-          <div
-            css={css`
-              padding: 8px;
-              background: rgba(255, 255, 255, 0.8);
-              box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-            `}
-          >
-            よって、ここに、国連総会は すべての人間は
-            加盟国は、国際連合と協力して
-          </div>
+          よって、ここに、国連総会は すべての人間は 加盟国は、国際連合と協力して
         </div>
-      </div>
+        <MobileScreenshot />
+      </MainContainer>
     </div>
   );
 }
