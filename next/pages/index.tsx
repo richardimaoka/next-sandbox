07418385f8a5a188d71004250121e3c063ea1bdd
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-24 08:16:34 +0900

    child box

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -3,7 +3,7 @@ import { Column } from "@/components/Column";
 import { MainContainer } from "@/components/MainContainer";
 import { css, keyframes } from "@emotion/react";
 import { Noto_Sans_JP } from "next/font/google";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const notoSansJP = Noto_Sans_JP({
   // Japanese font needs this settings, as index.d.ts doesn't allow subsets = japanese, which is probably due to the large size of japanese font
@@ -11,6 +11,60 @@ const notoSansJP = Noto_Sans_JP({
   display: "swap", // removing this will unapplied japanese font, BUT THIS CAUSES LAYOUT SHIFT...!!!
 });
 
+const ChildBox = () => {
+  const [content, setContent] = useState("");
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const box = ref.current;
+      const rect = box.getBoundingClientRect();
+      setContent(
+        `x: ${rect.x}, y: ${rect.y} width: ${rect.width} height: ${rect.height}`
+      );
+    }
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      css={css`
+        @media (max-width: 768px) {
+          width: 100vw;
+        }
+
+        height: 300px;
+        width: 400px;
+        margin: 10px 0;
+
+        background-color: #1e1e1e;
+        color: white;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      {content}
+    </div>
+  );
+};
+
 export default function Home() {
-  return <></>;
+  return (
+    <div
+      css={css`
+        @media (max-width: 768px) {
+          width: 100vw;
+          height: 100vh;
+        }
+
+        width: 768px;
+        height: 100vh;
+        overflow-y: auto;
+      `}
+    >
+      <ChildBox />
+    </div>
+  );
 }
