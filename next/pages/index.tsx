275ee8089ea38a10c7542c4615a6e0b363f0f74a
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-23 11:13:09 +0900

    clean up

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -11,59 +11,6 @@ const notoSansJP = Noto_Sans_JP({
   display: "swap", // removing this will unapplied japanese font, BUT THIS CAUSES LAYOUT SHIFT...!!!
 });
 
-const FlickeringTrail = () => {
-  const flickering = keyframes`
-          0% {
-            opacity: 1;
-          }
-          50% {
-            opacity: 0;
-          }
-          100% {
-            opacity: 1;
-          }
-  `;
-  return (
-    <span
-      css={css`
-        animation: ${flickering} 2s infinite;
-      `}
-    >
-      |
-    </span>
-  );
-};
 export default function Home() {
-  const command = "mkdir sign-in-with-google-experiment";
-  const [writtenLength, setWrittenLength] = useState(0);
-
-  useEffect(() => {
-    if (command && writtenLength < command.length) {
-      const incrementStep = command.length / 10;
-      const nextLength = Math.min(
-        writtenLength + incrementStep,
-        command.length
-      );
-      setTimeout(() => {
-        setWrittenLength(nextLength);
-      }, 20);
-    }
-  });
-
-  return (
-    <pre
-      css={css`
-        margin: 1px 0px;
-        padding: 4px;
-        background-color: #1e1e1e;
-        color: #f1f1f1;
-        border-bottom: 1px solid #333333;
-      `}
-    >
-      <code>
-        {command?.substring(0, writtenLength)}
-        <FlickeringTrail />
-      </code>
-    </pre>
-  );
+  return <></>;
 }
