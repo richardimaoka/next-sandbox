98d20b10140be2f249537408ab00273db6a85ae5
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-03 00:03:26 +0900

    column

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,29 +1,9 @@
-import { CurrentDirectory } from "./terminal/CurrentDirectory";
-import { TerminalCommand } from "./terminal/TerminalCommand";
-import { TerminalOutput } from "./terminal/TerminalOutput";
+import { TerminalColumn } from "./terminal/TerminalColumn";
 
 export default function Home() {
   return (
     <main>
-      <CurrentDirectory currentDirectory="~" />
-      <TerminalCommand
-        command="docker pull httpd"
-        fold={false}
-        animate={true}
-      />
-      <TerminalOutput
-        output={`Using default tag: latest
-latest: Pulling from library/httpd
-648e0aadf75a: Pull complete
-c76ba39af630: Pull complete
-b9819ffb14ec: Pull complete
-37baa60548e6: Pull complete
-6dbce5de7542: Pull complete
-Digest: sha256:d7262c0f29a26349d6af45199b2770d499c74d45cee5c47995a1ebb336093088
-Status: Downloaded newer image for httpd:latest
-docker.io/library/httpd:latest`}
-        fold={false}
-      />
+      <TerminalColumn />
     </main>
   );
 }
--- next/app/terminal/TerminalCommand.tsx
+++ next/app/terminal/TerminalCommand.tsx
@@ -1,5 +1,6 @@
 import { source_code_pro } from "../fonts/fonts";
 import { FlickeringTrail } from "./FlickeringTrail";
+import { TerminalPrompt } from "./TerminalPrompt";
 import styles from "./terminal.module.css";
 
 interface TerminalCommandProps {
@@ -16,6 +17,7 @@ export const TerminalCommand = ({
   const className = fold ? styles.command + " " + styles.fold : styles.command;
   return (
     <pre className={className}>
+      <TerminalPrompt />
       <code className={source_code_pro.className}>{command}</code>
       {animate && <FlickeringTrail />}
     </pre>
