5fd063c3a0ee81446256568f29ece18b722f27f7
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-02 23:04:58 +0900

    flickering

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { FlickeringTrail } from "./terminal/FlickeringTrail";
 
 export default function Home() {
-  return <main>this is the main</main>;
+  return (
+    <main>
+      <pre>
+        <code>
+          this is the main
+          <FlickeringTrail />
+        </code>
+      </pre>
+    </main>
+  );
 }
---
+++ next/app/terminal/FlickeringTrail.tsx
@@ -0,0 +1,5 @@
+import styles from "./terminal.module.css";
+
+export const FlickeringTrail = () => {
+  return <span className={styles.flickering}>|</span>;
+};
