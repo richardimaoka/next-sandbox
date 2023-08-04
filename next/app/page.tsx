d2f523c0067a903a6e16e0269571b8a317bbb82e
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-05 02:31:26 +0900

    files

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -2,17 +2,25 @@ import { Column } from "./components/column/Column";
 import { FileNodeComponent } from "./components/sourcecode/filetree/FileNodeComponent";
 
 export default function Home() {
+  const fileTree = [
+    { nodeType: "FILE", filePath: ".gitignore" },
+    { nodeType: "FILE", filePath: "index.html" },
+    { nodeType: "FILE", filePath: "package-lock.json" },
+    { nodeType: "FILE", filePath: "package.json" },
+  ];
   return (
     <main>
-      {/* <Column /> */}
-      <FileNodeComponent
-        nodeType="FILE"
-        name="index.tsx"
-        filePath="index.tsx"
-        offset={0}
-        isUpdated={false}
-        step="1"
-      />
+      {fileTree.map((file) => (
+        <FileNodeComponent
+          key={file.filePath}
+          nodeType={"FILE"}
+          name={file.filePath}
+          filePath={file.filePath}
+          offset={0}
+          isUpdated={false}
+          step="1"
+        />
+      ))}
     </main>
   );
 }
