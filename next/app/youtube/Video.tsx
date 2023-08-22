4f0b7188dac90001f8d4570c84348bff499cb536
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-23 06:43:11 +0900

    youtube

--- next/app/youtube/Video.tsx
+++ next/app/youtube/Video.tsx
@@ -1,12 +1,19 @@
 export const Video = () => {
   return (
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/xz6aeeeJR-g"
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen
-    ></iframe>
+    <div>
+      <div>
+        <button style={{ backgroundColor: "white", color: "black" }}>
+          button
+        </button>
+      </div>
+      <iframe
+        width="185"
+        height="324"
+        src="https://www.youtube.com/embed/xz6aeeeJR-g"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </div>
   );
 };
