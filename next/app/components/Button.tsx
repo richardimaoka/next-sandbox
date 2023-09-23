b3b8131a74b2f19ed92d567de08347d1e4335bea
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-23 10:40:07 +0900

    json stringify

--- next/app/components/Button.tsx
+++ next/app/components/Button.tsx
@@ -1,3 +1,13 @@
-export function Button() {
-  return <button>this is a buttton</button>;
+export async function Button() {
+  const data = await fetch("http://localhost:3002/posts");
+  const json = await data.json();
+
+  return (
+    <div>
+      <pre>
+        <code>{JSON.stringify(json)}</code>
+      </pre>
+      <button>this is a buttton</button>
+    </div>
+  );
 }
