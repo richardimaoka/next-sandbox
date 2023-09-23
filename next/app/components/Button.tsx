18ca5e6c8b8ada54177a856584d9b98997c57e53
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-23 10:42:41 +0900

    server action

--- next/app/components/Button.tsx
+++ next/app/components/Button.tsx
@@ -2,12 +2,27 @@ export async function Button() {
   const data = await fetch("http://localhost:3002/posts");
   const json = await data.json();
 
+  async function postNew() {
+    "use server";
+    fetch("http://localhost:3002/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+  }
+
   return (
-    <div>
+    <form action={postNew}>
       <pre>
         <code>{JSON.stringify(json)}</code>
       </pre>
       <button>this is a buttton</button>
-    </div>
+    </form>
   );
 }
