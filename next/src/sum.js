41a5a9844c02cc12dd5ead8e905836337063e63d
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-10 20:51:57 +0900

    more assertions

--- next/src/sum.js
+++ next/src/sum.js
@@ -1,4 +1,13 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+module.exports = {
+  sum: function sum(a, b) {
+    return a + b;
+  },
+
+  fetchData: async function fetchData() {
+    return "peanut butter";
+  },
+
+  fetchDataE: async function fetchDataE() {
+    throw "error";
+  },
+};
--- next/src/sum.test.js
+++ next/src/sum.test.js
@@ -1,4 +1,7 @@
-const sum = require("./sum");
+const pkg = require("./sum");
+const sum = pkg.sum;
+const fetchData = pkg.fetchData;
+const fetchDataE = pkg.fetchDataE;
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -12,4 +15,18 @@ test("object assignment", () => {
   const data = { one: 1 };
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("the data is peanut butter", async () => {
+  const data = await fetchData();
+  expect(data).toBe("peanut butter");
+});
+
+test("the fetch fails with an error", async () => {
+  expect.assertions(1);
+  try {
+    await fetchDataE();
+  } catch (e) {
+    expect(e).toMatch("error");
+  }
 });
