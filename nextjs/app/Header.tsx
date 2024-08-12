7bc28f6cc39f29990e46f27f3d5be7c68777b6ed
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-13 01:28:22 +0900

    search popover

--- nextjs/app/Header.tsx
+++ nextjs/app/Header.tsx
@@ -7,19 +7,18 @@ export function Header(props: Props) {
   return (
     <div className={styles.component}>
       <div className={styles.logo}>Logo</div>
-      <Link className={styles.link} href="/link1">
-        link1
-      </Link>
-      <Link className={styles.link} href="/link2">
-        link2
-      </Link>
-      <Link className={styles.link} href="/link3">
-        link3
-      </Link>
       <div className={styles.search}>
-        <input className={styles.input} placeholder="search text" />
+        <button popoverTarget="search-popover" className={styles.button}>
+          search text
+        </button>
+        <div
+          id="search-popover"
+          className={styles.popover}
+          popover="auto"
+        >
+          a
+        </div>
       </div>
-      <button className={styles.login}>Log in</button>
     </div>
   );
 }
