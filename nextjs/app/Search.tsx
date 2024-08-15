1ea4c9809158f518bef464df9a4f19153586e3a5
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-16 08:26:04 +0900

    expand

--- nextjs/app/Search.tsx
+++ nextjs/app/Search.tsx
@@ -3,19 +3,5 @@ import styles from "./Search.module.css";
 interface Props {}
 
 export function Search(props: Props) {
-  return (
-    <div className={styles.component}>
-      <button popoverTarget="search-popover" className={styles.button}>
-        search text
-      </button>
-
-      <div id="search-popover" className={styles.popover} popover="auto">
-        <input className={styles.input} placeholder="search text" autoFocus />
-        <div className={styles.item}>result 1</div>
-        <div className={styles.item}>result 2</div>
-        <div className={styles.item}>result 3</div>
-        <div className={styles.item}>result 4</div>
-      </div>
-    </div>
-  );
+  return <input className={styles.input} placeholder="search text" />;
 }
