1c415a78bddd36d48f01b118bc5ae21df9a934bf
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-17 15:21:33 +0900

    header items

--- nextjs/app/Header.tsx
+++ nextjs/app/Header.tsx
@@ -1,12 +1,15 @@
 import styles from "./Header.module.css";
-import { Logo } from "./Logo";
 
 interface Props {}
 
 export function Header(props: Props) {
   return (
     <div className={styles.component}>
-      <Logo />
+      <div className={styles.logo}>logo</div>
+      <div>item1</div>
+      <div>item2</div>
+      <div>item3</div>
+      <div>item4</div>
     </div>
   );
 }
