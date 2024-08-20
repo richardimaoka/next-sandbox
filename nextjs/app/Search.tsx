57b15b95da32d300856ae86f9be67d4eb6b241f1
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-20 20:21:35 +0900

    calling not-yet implemented router handler

--- nextjs/app/Search.tsx
+++ nextjs/app/Search.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./Search.module.css";
+
+interface SearchResult {
+  id: string;
+  name: string;
+}
 
 interface Props {}
 
 export function Search(props: Props) {
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
+  async function onChange(searchText: string) {
+    const response = await fetch(`http://localhost:8080/query=${searchText}`);
+    const results = (await response.json()) as SearchResult[];
+    setSearchResults(results);
+  }
+
   return (
     <div className={styles.component}>
       <button popoverTarget="search-popover" className={styles.button}>
@@ -10,11 +26,17 @@ export function Search(props: Props) {
       </button>
 
       <div id="search-popover" className={styles.popover} popover="auto">
-        <input className={styles.input} placeholder="search text" autoFocus />
-        <div className={styles.item}>result 1</div>
-        <div className={styles.item}>result 2</div>
-        <div className={styles.item}>result 3</div>
-        <div className={styles.item}>result 4</div>
+        <input
+          className={styles.input}
+          placeholder="search text"
+          autoFocus
+          onChange={(e) => console.log(e.target.value)}
+        />
+        {searchResults.map((r) => (
+          <div key={r.id} className={styles.item}>
+            {r.name}
+          </div>
+        ))}
       </div>
     </div>
   );
