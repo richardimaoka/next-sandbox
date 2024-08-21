218502c7f83a777b54827fc30d25d22c276f77ac
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-21 23:09:08 +0900

    expand with button

--- nextjs/app/Search.tsx
+++ nextjs/app/Search.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import styles from "./Search.module.css";
 
 interface Props {}
 
 export function Search(props: Props) {
-  return <input className={styles.input} placeholder="search text" />;
+  return (
+    <form className={styles.component}>
+      <input className={styles.input} placeholder="search text" />
+      <button className={styles.button}>search</button>
+    </form>
+  );
 }
