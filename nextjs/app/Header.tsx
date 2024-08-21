18fbce3e4e70164b4833e24c21ebcfd3e3a02bf2
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-21 22:31:10 +0900

    search results right below

--- nextjs/app/Header.tsx
+++ nextjs/app/Header.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useState } from "react";
 import styles from "./Header.module.css";
 import { Logo } from "./Logo";
 import { Search } from "./Search";
@@ -8,19 +5,9 @@ import { Search } from "./Search";
 interface Props {}
 
 export function Header(props: Props) {
-  const [searchMode, setSearchMode] = useState(false);
-
   return (
-    <div
-      className={
-        styles.component + (searchMode ? " " + styles.searchExpand : "")
-      }
-    >
+    <div className={styles.component}>
       <Logo />
-      {!searchMode && <div>item1</div>}
-      {!searchMode && <div>item2</div>}
-      {!searchMode && <div>item3</div>}
-      {!searchMode && <div>item4</div>}
       <Search />
     </div>
   );
--- nextjs/app/Search.tsx
+++ nextjs/app/Search.tsx
@@ -14,14 +14,17 @@ export function Search(props: Props) {
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
 
   async function onChange(searchText: string) {
-    const response = await fetch(`http://localhost:8080/query=${searchText}`);
+    const response = await fetch(
+      `http://localhost:3000/api/search?query=${searchText}`
+    );
     const results = (await response.json()) as SearchResult[];
     setSearchResults(results);
   }
 
   return (
     <div className={styles.component}>
-      <button popoverTarget="search-popover" className={styles.button}>
+      {/* @ts-ignore: Did you mean 'popoverTarget'?ts(2322), but changing to 'T'arget will cause a different warning */}
+      <button popovertarget="search-popover" className={styles.button}>
         search text
       </button>
 
@@ -30,7 +33,7 @@ export function Search(props: Props) {
           className={styles.input}
           placeholder="search text"
           autoFocus
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => onChange(e.target.value)}
         />
         {searchResults.map((r) => (
           <div key={r.id} className={styles.item}>
