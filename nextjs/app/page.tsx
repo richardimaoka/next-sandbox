524436537d4bb385af61d108baafeed40381233b
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-07 06:55:22 +0900

    update

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,10 +1,20 @@
-import { Header } from "./Header";
 import styles from "./page.module.css";
 
+async function f() {
+  return [2, true];
+}
+
+async function g() {
+  const [n, b] = await f();
+
+  console.log(n);
+  console.log(b);
+
+  const res = await f();
+  console.log(res);
+}
+
 export default function Page() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  g();
+  return <div>page</div>;
 }
