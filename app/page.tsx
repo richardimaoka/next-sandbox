a6e1c3c61499fc74fefc6ab8d4c4a1d0ef313c02
richardimaoka <richard.s.imaoka@gmail.com>
2025-05-05 03:51:29 +0900

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import { Breadcrumbs } from "./components/link/Breadcrumbs";
 import styles from "./page.module.css";
 
 export default function Page() {
-  return (
-    <div className={styles.component}>
-      <Breadcrumbs
-        elements={[
-          { text: "Home", href: "/" },
-          { text: "items", href: "/" },
-          { text: "new item", href: "/" },
-        ]}
-      />
-    </div>
-  );
+  return <div className={styles.component}></div>;
 }
