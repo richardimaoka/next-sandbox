dcea47a27b19d7edda8e85b514afb555f93e336f
richardimaoka <richard.s.imaoka@gmail.com>
2025-12-27 21:03:35 +0900

    exepriment view transition enter only

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>This is the home page</div>;
+  return (
+    <div>
+      This is the home page <Link href="/parent">go to parent</Link>
+    </div>
+  );
 }
--- app/parent/page.tsx
+++ app/parent/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import { ViewTransition } from "react";
+
 export default function Home() {
-  return <div>This is the parent page</div>;
+  return (
+    <div>
+      This is the parent page. <Link href="/">go to home</Link>
+      <ViewTransition>
+        <div
+          style={{
+            backgroundColor: "lightblue",
+            width: "200px",
+            height: "100px",
+          }}
+        ></div>
+      </ViewTransition>
+    </div>
+  );
 }
