464b8cfad0ee99cde4b17c5d722b711d82e73aa7
richardimaoka <richard.s.imaoka@gmail.com>
2025-12-31 21:39:59 +0900

    event handler

--- app/client.tsx
+++ app/client.tsx
@@ -1,14 +1,37 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function Client() {
+  const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     console.log("Client useEffect called");
     // throw new Error("Client useEffect error");
+
+    const divElem = ref.current;
+    if (divElem) {
+      const handler = () => {
+        throw new Error("event handler error");
+      };
+
+      divElem.addEventListener("click", handler);
+      return () => {
+        divElem.removeEventListener("click", handler);
+      };
+    }
   });
 
-  throw new Error("Client rendering error");
+  // throw new Error("Client rendering error");
 
-  return <div className={styles.component}></div>;
+  return (
+    <div
+      ref={ref}
+      // onClick={() => {
+      //   throw new Error("onClick error");
+      // }}
+    >
+      client
+    </div>
+  );
 }
